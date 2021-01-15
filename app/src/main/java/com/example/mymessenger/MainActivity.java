de4505c1f455214c6954f9d1fb79982b77acee64
alejandrogualdron <alejandrogualdron@users.noreply.github.com>
2021-01-15 01:13:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mymessenger/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    //call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
+        EditText messageView=(EditText)findViewById(R.id.message);
+        String messageText=messageView.getText().toString();
+        //Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        Intent intent= new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello");
+        startActivity(intent);
+    }
+}
