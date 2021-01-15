a2d780fc75a2569dcc6d943014faa1a8dd9034cb
alejandrogualdron <alejandrogualdron@users.noreply.github.com>
2021-01-15 13:14:23 -0500

    Laboratorio Finalizado

--- app/src/main/java/com/example/mymessenger/MainActivity.java
+++ app/src/main/java/com/example/mymessenger/MainActivity.java
@@ -1,27 +1,31 @@
 package com.example.mymessenger;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
 import android.widget.EditText;
+
+import co.edu.unipiloto.mymessenger.R;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
-    //call onSendMessage() when the button is clicked
-    public void onSendMessage(View view) {
-        EditText messageView=(EditText)findViewById(R.id.message);
-        String messageText=messageView.getText().toString();
-        //Intent intent = new Intent(this,ReceiveMessageActivity.class);
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        // Intent intent = new Intent(this, ReceiveMessageActivity.class);
         //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
-        Intent intent= new Intent(Intent.ACTION_SEND);
+        Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, "Hello");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        //intent.putExtra(Intent.EXTRA_TEXT,"Hello");
         startActivity(intent);
     }
 }
